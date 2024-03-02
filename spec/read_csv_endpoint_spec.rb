280481ca759909c43d35a8d57e1ac1c4cbf008e5
Danilo Martins <dansm86@outlook.com>
2024-03-02 14:24:04 -0300

    test/mocked csv read return

--- spec/read_csv_endpoint_spec.rb
+++ spec/read_csv_endpoint_spec.rb
@@ -8,9 +8,21 @@ RSpec.describe 'Server' do
     Sinatra::Application
   end
 
-  it 'gets the csv data by the endpoint /tests' do
-    response = get '/tests'
-    data = JSON.parse(response.body)
-    expect(data).to be_instance_of Array
+  describe 'GET /tests' do
+    it 'returns the csv data' do
+      allow(CSV).to receive(:read).with("data/data.csv", col_sep: ';').and_return([
+        ['cpf', 'name'], ['1234567890', 'John Doe'], ['0987654321', 'Jane Smith']
+      ])
+
+      response = get '/tests'
+
+      data = JSON.parse(response.body)
+
+      expect(data).to be_instance_of Array
+      expect(data).to eq([
+        { 'cpf' => '1234567890', 'name' => 'John Doe' },
+        { 'cpf' => '0987654321', 'name' => 'Jane Smith' }
+      ])
+    end
   end
 end
