c65e7ca27317b8f00356473f1ca1681a40ab0c7b
Danilo Martins <dansm86@outlook.com>
2024-03-07 11:48:00 -0300

    fix/fixed the prepare and execute methods
    - moved the prepare statements out of the CSV loop
    - corrected the method call and its arguments;
    - increased time to verify the DB healthy in compose file

--- app/backend/src/database/database_setup.rb
+++ app/backend/src/database/database_setup.rb
@@ -39,42 +39,43 @@ DBManager.conn.exec("
 
 puts 'Populating the tables with data...'
 
+DBManager.conn.prepare("insert_patient", "
+  INSERT INTO patients (cpf, name, email, birth_date, address, city, state)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)
+  ON CONFLICT (cpf) DO NOTHING
+")
+
+DBManager.conn.prepare("insert_doctor", "
+  INSERT INTO doctors (crm, crm_state, name, email)
+  VALUES ($1, $2, $3, $4)
+  ON CONFLICT (crm) DO NOTHING
+")
+
+DBManager.conn.prepare("insert_exam", "
+  INSERT INTO exams (patient_cpf, doctor_crm, token, exam_date, exam_type, exam_limits, exam_result)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)
+")
+
 CSV.foreach('src/data/data.csv', headers: true, col_sep: ';') do |row|
-  insert_patient_statement = DBManager.conn.prepare("
-    INSERT INTO patients (cpf, name, email, birth_date, address, city, state)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
-    ON CONFLICT (cpf) DO NOTHING
-  ")
 
-  insert_doctor_statement = DBManager.conn.prepare("
-    INSERT INTO doctors (crm, crm_state, name, email)
-    VALUES ($1, $2, $3, $4)
-    ON CONFLICT (crm) DO NOTHING
-  ")
+  DBManager.conn.exec_prepared("insert_patient", [
+      row['cpf'],
+      row['nome paciente'],
+      row['email paciente'],
+      row['data nascimento paciente'],
+      row['endereço/rua paciente'],
+      row['cidade paciente'],
+      row['estado patiente']
+  ]);
 
-  insert_exam_statement = DBManager.conn.prepare("
-    INSERT INTO exams (patient_cpf, doctor_crm, token, exam_date, exam_type, exam_limits, exam_result)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
-  ")
-
-  insert_patient_statement.execute(
-    row['cpf'],
-    row['nome paciente'],
-    row['email paciente'],
-    row['data nascimento paciente'],
-    row['endereço/rua paciente'],
-    row['cidade paciente'],
-    row['estado patiente']
-  )
-
-  insert_doctor_statement.execute(
+  DBManager.conn.exec_prepared("insert_doctor", [
     row['crm médico'],
     row['crm médico estado'],
     row['nome médico'],
     row['email médico']
-  )
+  ]);
 
-  insert_exam_statement.execute(
+  DBManager.conn.exec_prepared("insert_exam", [
     row['cpf'],
     row['crm médico'],
     row['token resultado exame'],
@@ -82,7 +83,7 @@ CSV.foreach('src/data/data.csv', headers: true, col_sep: ';') do |row|
     row['tipo exame'],
     row['limites tipo exame'],
     row['resultado tipo exame']
-  )
+  ]);
 end
 
 puts 'Done'
