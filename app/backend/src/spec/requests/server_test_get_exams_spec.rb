c90bb0d62310d08a3618fdc8c2e8e8fb67993ba9
Danilo Martins <dansm86@outlook.com>
2024-03-14 10:48:21 -0300

    refactored requests tests
    - declared variables as let at the start of the test

--- app/backend/src/spec/requests/server_test_get_exams_spec.rb
+++ app/backend/src/spec/requests/server_test_get_exams_spec.rb
@@ -5,19 +5,21 @@ require_relative '../support/test_data.rb'
 RSpec.describe 'Server' do
   describe 'GET /tests' do
     let(:mock_conn) { double('PG::Connection') }
+    let(:test_query) {
+      <<~SQL.gsub("\n", " ")
+        SELECT p.*, d.*, e.*, t.*
+        FROM exams e
+        JOIN patients p ON p.cpf = e.patient_cpf
+        JOIN doctors d ON d.crm = e.doctor_crm
+        JOIN tests t ON t.exam_token = e.token
+      SQL
+    }
 
     before do
       allow(DBManager).to receive(:conn).and_return(mock_conn)
     end
     it 'returns the exams data' do
-      query = <<~SQL.gsub("\n", " ")
-        SELECT p.*, d.*, e.*
-        FROM exams e
-        JOIN patients p ON p.cpf = e.patient_cpf
-        JOIN doctors d ON d.crm = e.doctor_crm
-      SQL
-
-      allow(mock_conn).to receive(:exec_params).with("#{query};", []).and_return(db_result)
+      allow(mock_conn).to receive(:exec_params).with("#{test_query};", []).and_return(db_result)
 
       response = get '/tests'
 
@@ -29,14 +31,7 @@ RSpec.describe 'Server' do
 
     context 'with token params' do
       it 'returns the specific exams data' do
-        query = <<~SQL.gsub("\n", " ")
-          SELECT p.*, d.*, e.*
-          FROM exams e
-          JOIN patients p ON p.cpf = e.patient_cpf
-          JOIN doctors d ON d.crm = e.doctor_crm
-        SQL
-
-        allow(mock_conn).to receive(:exec_params).with("#{query} WHERE e.token = $1;", ["IQCZ17"]).and_return(db_result[0..1])
+        allow(mock_conn).to receive(:exec_params).with("#{test_query} WHERE e.token = $1;", ["IQCZ17"]).and_return(db_result[0..1])
 
         response = get '/tests/IQCZ17'
 
