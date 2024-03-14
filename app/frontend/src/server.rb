3d209769fe93daf78f85efcec48724ee14a32934
Danilo Martins <dansm86@outlook.com>
2024-03-14 15:33:24 -0300

    feat/send csv file via input element in page
    - added faraday/multipart to upload files in the server
    - added input file type element in views
    - created request and system tests

--- app/frontend/src/server.rb
+++ app/frontend/src/server.rb
@@ -1,6 +1,7 @@
 require 'rack/handler/puma'
 require 'sinatra'
 require 'faraday'
+require 'faraday/multipart'
 
 get '/' do
   erb :index
@@ -14,6 +15,7 @@ get '/search' do
   erb :index
 end
 
+# This route is used to get the data from the backend
 get '/data' do
   content_type :json
 
@@ -28,6 +30,49 @@ get '/data' do
   end
 
   response.body
+end
+
+# This route is used to upload the CSV file to the backend
+post '/upload' do
+  content_type :json
+
+  puts 'Params:'
+  puts params.inspect
+
+  unless params[:csvFile] && params[:csvFile] != 'undefined' &&
+    (tmpfile = params[:csvFile][:tempfile]) &&
+    (name = params[:csvFile][:filename])
+
+    status 400
+    return { success: false, message: 'No file was uploaded' }.to_json
+  end
+
+  unless name.match?(/\.csv\z/)
+    status 400
+    return { success: false, message: 'Invalid file format' }.to_json
+  end
+
+  begin
+    conn = Faraday.new(url: 'http://backend:3001') do |builder|
+      builder.request :multipart
+      builder.request :url_encoded
+      builder.adapter :net_http
+    end
+
+    payload = { file: Faraday::Multipart::FilePart.new(tmpfile, 'text/csv') }
+
+    response = conn.post('import', payload)
+
+    if response.status == 200
+        status 200
+        { success: true, message: 'Data imported successfully' }.to_json
+    end
+  rescue => e
+    puts e.message
+
+    status 500
+    { error: true,  message: 'An error occurred while importing data. Try again' }.to_json
+  end
 end
 
 unless ENV['RACK_ENV'] == 'test'
