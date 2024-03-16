c0eb129b481bc9b4c4ac0a14f1b8f3b7cdf288b6
Danilo Martins <dansm86@outlook.com>
2024-03-16 00:38:13 -0300

    test/refact/updated get data tests
    - refactored the get exams endpoint tests

--- app/frontend/src/spec/requests/server_get_data_spec.rb
+++ app/frontend/src/spec/requests/server_get_data_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../spec_helper.rb'
 require_relative '../support/test_data.rb'
+require_relative '../../services/api_service.rb'
 require 'json'
 require 'faraday'
 
@@ -9,10 +10,19 @@ RSpec.describe 'Server' do
   end
 
   describe 'GET /data' do
+    let(:mock_conn) { instance_double(Faraday::Connection) }
+    let(:mock_response) { instance_double(Faraday::Response) }
+
+    before do
+      allow(ApiService).to receive(:connection).and_return(mock_conn)
+    end
+
     it 'returns the exams data' do
-      conn = instance_double(Faraday::Connection)
-      allow(Faraday).to receive(:new).and_return(conn)
-      allow(conn).to receive(:get).with('tests').and_return(double(body: api_response.to_json))
+      api_response_json = api_response.to_json
+
+      allow(ApiService).to receive(:get_exams).with(mock_conn).and_return(mock_response)
+      allow(mock_response).to receive(:body).and_return(api_response_json)
+      allow(mock_response).to receive(:status).and_return(200)
 
       response = get '/data'
 
@@ -20,23 +30,63 @@ RSpec.describe 'Server' do
 
       expect(response.status).to eq 200
       expect(data).to be_instance_of Array
-      expect(data).to eq(JSON.parse(api_response.to_json))
+      expect(data).to eq(JSON.parse(api_response_json))
     end
 
     context 'with token params' do
       it 'returns the specific exams data' do
-        conn = instance_double(Faraday::Connection)
-        allow(Faraday).to receive(:new).and_return(conn)
-        allow(conn).to receive(:get).with("tests/IQCZ17").and_return(double(body: api_response.to_json))
+        api_response_json = [api_response[0]].to_json
+        token = 'IQCZ17'
 
-        response = get '/data?token=IQCZ17'
+        allow(ApiService).to receive(:get_exam_by_token).with(mock_conn, token).and_return(mock_response)
+        allow(mock_response).to receive(:body).and_return(api_response_json)
+        allow(mock_response).to receive(:status).and_return(200)
+
+        response = get "/data?token=#{token}"
 
         data = JSON.parse(response.body)
 
         expect(response.status).to eq 200
         expect(data).to be_instance_of Array
-        expect(data).to eq(JSON.parse(api_response.to_json))
+        expect(data).to eq(JSON.parse(api_response_json))
       end
     end
+
+    context "with unregistered token" do
+      it "returns an empty array" do
+        api_response_json = [].to_json
+        token = 'blabla'
+
+        allow(ApiService).to receive(:get_exam_by_token).with(mock_conn, token).and_return(mock_response)
+        allow(mock_response).to receive(:body).and_return(api_response_json)
+        allow(mock_response).to receive(:status).and_return(200)
+
+        response = get "/data?token=#{token}"
+
+        data = JSON.parse(response.body)
+
+        expect(response.status).to eq 200
+        expect(data).to be_instance_of Array
+        expect(data).to eq(JSON.parse(api_response_json))
+      end
+    end
+
+    context "when occurs server error" do
+      it "returns an error message at /data endpoint" do
+        api_response_json = { error: true,  message: 'An error has occurred. Try again' }.to_json
+
+        allow(ApiService).to receive(:get_exams).and_raise(Faraday::ServerError)
+        allow(mock_response).to receive(:body).and_return(api_response_json)
+        allow(mock_response).to receive(:status).and_return(500)
+
+        response = get '/data'
+
+        data = JSON.parse(response.body)
+
+        expect(response.status).to eq 500
+        expect(data).to eq(JSON.parse(api_response_json))
+      end
+    end
+
   end
 end
